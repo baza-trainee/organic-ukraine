b762d865ae9df674e933433994b5881b280dd903
SerhiiOberemchuk <gardenitparts@gmail.com>
2024-03-02 10:39:20 +0100

    feat: fonts, style, component Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,23 @@
-
-import "./styles/App.css";
-import { Header } from "./components/Header/Header";
-import { Hero } from "./components/Hero/Hero";
-import { WhatWeDo } from "./components/WhatWeDo/WhatWeDo";
-import { OrganicGoods } from "./components/OrganicGoods/OrganicGoods";
-import { ArticlesAndTips } from "./components/ArticlesAndTips/ArticlesAndTips";
-import { OurPartners } from "./components/OurPartners/OurPartners";
-import { Footer } from "./components/Footer/Footer";
-import { FarmLocationsMap } from "./components/FarmLocationsMap/FarmLocationsMap";
-
+import { Header } from './components/Header/Header';
+import { Hero } from './components/Hero/Hero';
+import { WhatWeDo } from './components/WhatWeDo/WhatWeDo';
+import { OrganicGoods } from './components/OrganicGoods/OrganicGoods';
+import { ArticlesAndTips } from './components/ArticlesAndTips/ArticlesAndTips';
+import { OurPartners } from './components/OurPartners/OurPartners';
+import { Footer } from './components/Footer/Footer';
+import { FarmLocationsMap } from './components/FarmLocationsMap/FarmLocationsMap';
 
 function App() {
-
-
   return (
     <>
       <Header />
       <Hero />
       <WhatWeDo />
       <OrganicGoods />
-      <ArticlesAndTips/>
+      <ArticlesAndTips />
       <OurPartners />
-      <FarmLocationsMap/>
-      <Footer/>
+      <FarmLocationsMap />
+      <Footer />
     </>
   );
 }
