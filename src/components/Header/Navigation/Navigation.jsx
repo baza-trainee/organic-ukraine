87c70d639242db76ec2692667de22c7a826a9e17
SerhiiOberemchuk <gardenitparts@gmail.com>
2024-03-03 16:40:23 +0100

    feat: refractor-select-lang

--- src/components/Header/Navigation/Navigation.jsx
+++ src/components/Header/Navigation/Navigation.jsx
@@ -1,24 +1,35 @@
 import { Link } from 'react-scroll';
 
 import css from './Navigation.module.css';
+import { useTranslation } from 'react-i18next';
 
 const itemsNavigation = [
-  { name: 'Що ми робимо', id: 'WhatWeDo' },
-  { name: 'Продукти', id: 'OrganicGoods' },
-  { name: 'Статті', id: 'ArticlesAndTips' },
-  { name: 'Партнери', id: 'OurPartners' },
-  { name: 'Мапа господарств', id: 'FarmLocationsMap' },
-  { name: 'Контакти', id: 'Footer' },
+  { name: 'WhatWeDo', id: 'WhatWeDo' },
+  { name: 'OrganicGoods', id: 'OrganicGoods' },
+  { name: 'ArticlesAndTips', id: 'ArticlesAndTips' },
+  { name: 'OurPartners', id: 'OurPartners' },
+  { name: 'FarmLocationsMap', id: 'FarmLocationsMap' },
+  { name: 'Footer', id: 'Footer' },
 ];
+// const itemsNavigation = [
+//   { name: 'Що ми робимо', id: 'WhatWeDo' },
+//   { name: 'Продукти', id: 'OrganicGoods' },
+//   { name: 'Статті', id: 'ArticlesAndTips' },
+//   { name: 'Партнери', id: 'OurPartners' },
+//   { name: 'Мапа господарств', id: 'FarmLocationsMap' },
+//   { name: 'Контакти', id: 'Footer' },
+// ];
 
 export const Navigation = () => {
+  const { t } = useTranslation('Header');
   return (
     <nav className={css.navigation}>
       <ul className={css.list}>
         {itemsNavigation.map(({ name, id }) => (
           <li key={id}>
             <Link to={id} smooth="true" duration={500} className={css.link}>
-              {name}
+              {/* {name} */}
+              {t(name)} {/* Translate the name using t function */}
             </Link>
           </li>
         ))}
