1bf4e16aa562c182984dd5db4cc6bec6a67d4ead
SerhiiOberemchuk <gardenitparts@gmail.com>
2024-03-03 20:03:29 +0100

    fix: select lang

--- src/components/Header/Navigation/Navigation.jsx
+++ src/components/Header/Navigation/Navigation.jsx
@@ -4,32 +4,23 @@ import css from './Navigation.module.css';
 import { useTranslation } from 'react-i18next';
 
 const itemsNavigation = [
-  { name: 'WhatWeDo', id: 'WhatWeDo' },
-  { name: 'OrganicGoods', id: 'OrganicGoods' },
-  { name: 'ArticlesAndTips', id: 'ArticlesAndTips' },
-  { name: 'OurPartners', id: 'OurPartners' },
-  { name: 'FarmLocationsMap', id: 'FarmLocationsMap' },
-  { name: 'Footer', id: 'Footer' },
+  { name: 'WhatWeDo' },
+  { name: 'OrganicGoods' },
+  { name: 'ArticlesAndTips' },
+  { name: 'OurPartners' },
+  { name: 'FarmLocationsMap' },
+  { name: 'Footer' },
 ];
-// const itemsNavigation = [
-//   { name: 'Що ми робимо', id: 'WhatWeDo' },
-//   { name: 'Продукти', id: 'OrganicGoods' },
-//   { name: 'Статті', id: 'ArticlesAndTips' },
-//   { name: 'Партнери', id: 'OurPartners' },
-//   { name: 'Мапа господарств', id: 'FarmLocationsMap' },
-//   { name: 'Контакти', id: 'Footer' },
-// ];
 
 export const Navigation = () => {
   const { t } = useTranslation('Header');
   return (
     <nav className={css.navigation}>
       <ul className={css.list}>
-        {itemsNavigation.map(({ name, id }) => (
-          <li key={id}>
-            <Link to={id} smooth="true" duration={500} className={css.link}>
-              {/* {name} */}
-              {t(name)} {/* Translate the name using t function */}
+        {itemsNavigation.map(({ name }) => (
+          <li key={name}>
+            <Link to={name} smooth="true" duration={500} className={css.link}>
+              {t(name)}
             </Link>
           </li>
         ))}
