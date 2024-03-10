d0d89437eb6039dc2be32526ffe326d3b817939d
SerhiiOberemchuk <gardenitparts@gmail.com>
2024-03-10 20:19:16 +0100

    feat: add map component

--- src/components/Header/Navigation/Navigation.jsx
+++ src/components/Header/Navigation/Navigation.jsx
@@ -4,12 +4,12 @@ import { Link } from 'react-scroll';
 import { useTranslation } from 'react-i18next';
 
 const itemsNavigation = [
-  { name: 'WhatWeDo' },
-  { name: 'OrganicGoods' },
-  { name: 'ArticlesAndTips' },
-  { name: 'FarmLocationsMap' },
-  { name: 'OurPartners' },
-  { name: 'Footer' },
+  'WhatWeDo',
+  'OrganicGoods',
+  'ArticlesAndTips',
+  'FarmLocationsMap',
+  'OurPartners',
+  'Footer',
 ];
 
 export const Navigation = ({ css }) => {
@@ -17,8 +17,8 @@ export const Navigation = ({ css }) => {
   return (
     <nav className={css.navigation}>
       <ul className={css.list}>
-        {itemsNavigation.map(({ name }) => (
-          <li key={name}>
+        {itemsNavigation.map((name, index) => (
+          <li key={index}>
             <Link to={name} smooth="true" duration={500} className={css.link}>
               {t(name)}
             </Link>
