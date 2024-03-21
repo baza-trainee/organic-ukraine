71bca0fdde73aa5e2a1f06acac1a792b441bb21f
SerhiiOberemchuk <gardenitparts@gmail.com>
2024-03-22 00:23:29 +0100

    feat:del fonts

--- src/App.jsx
+++ src/App.jsx
@@ -10,29 +10,6 @@ import { ScrollUp } from './components/Common/ScrollUp/ScrollUp';
 import { useTranslation } from 'react-i18next';
 import { useEffect } from 'react';
 import Favicon from 'react-favicon';
-// import { Suspense } from 'react';
-// import React from 'react';
-// const Header = React.lazy(() => import('../src/components/Header/Header'));
-// const Hero = React.lazy(() => import('../src/components/Hero/Hero'));
-// const WhatWeDo = React.lazy(
-//   () => import('../src/components/WhatWeDo/WhatWeDo')
-// );
-// const OrganicGoods = React.lazy(
-//   () => import('../src/components/OrganicGoods/OrganicGoods')
-// );
-// const ArticlesAndTips = React.lazy(
-//   () => import('../src/components/ArticlesAndTips/ArticlesAndTips')
-// );
-// const OurPartners = React.lazy(
-//   () => import('../src/components/OurPartners/OurPartners')
-// );
-// const Footer = React.lazy(() => import('../src/components/Footer/Footer'));
-// const FarmLocationsMap = React.lazy(
-//   () => import('../src/components/FarmLocationsMap/FarmLocationsMap')
-// );
-// const ScrollUp = React.lazy(
-//   () => import('../src/components/Common/ScrollUp/ScrollUp')
-// );
 
 function App() {
   const { i18n } = useTranslation();
@@ -41,20 +18,6 @@ function App() {
   }, [i18n.language]);
 
   return (
-    // <>
-    //   <Suspense fallback={<div>Loading...</div>}>
-    //     <Header />
-    //     <Hero />
-    //     <WhatWeDo />
-    //     <OrganicGoods />
-    //     <ArticlesAndTips />
-    //     <OurPartners />
-    //     <FarmLocationsMap />
-    //     <Footer />
-    //     <ScrollUp />
-    //   </Suspense>
-    // </>
-
     <>
       <Favicon url="favicon.ico" />
       <Header />
