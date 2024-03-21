70c9dad9eb4a290f9922ac95de1550757678e77c
SerhiiOberemchuk <gardenitparts@gmail.com>
2024-03-22 00:14:24 +0100

    feat:add style modal window

--- src/components/Header/MobileMenu/MobileMenu.jsx
+++ src/components/Header/MobileMenu/MobileMenu.jsx
@@ -1,13 +1,26 @@
-import styles from './MenuStyle.module.css';
+import { LogoOrganic } from '../../Common/Logo/LogoOrganic';
+import { SupportTheProject } from '../../Common/SupportTheProject/SupportTheProject';
+import { Navigation } from '../Navigation/Navigation';
+import { SelectLanguage } from '../SelectLanguage/SelectLanguage';
+import styles from './MenuStyle.module.scss';
 
 export const MobileMenu = ({ isMenu }) => {
   return (
     <div className={styles.menu_section}>
       <div className={styles.menu}>
-        MobileMenu
-        <button type="button" onClick={isMenu} className={styles.close_menu}>
-          close
-        </button>
+        <div className={styles.menu_content}>
+          <button
+            type="button"
+            onClick={isMenu}
+            className={styles.close_menu}
+          ></button>
+          <LogoOrganic />
+          <Navigation css={styles} />
+          <div className={styles.support_box}>
+            <SupportTheProject />
+            <SelectLanguage />
+          </div>
+        </div>
       </div>
     </div>
   );
